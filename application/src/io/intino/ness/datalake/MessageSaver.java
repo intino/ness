6524bd2cb419b14702a3021826afffd5e8442049
Octavio Roncal <octavioroncal@monentia.es>
2018-04-01 18:57:46 +0100

    udpated saver

--- application/src/io/intino/ness/datalake/MessageSaver.java
+++ application/src/io/intino/ness/datalake/MessageSaver.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 import static java.nio.file.StandardOpenOption.APPEND;
@@ -18,19 +17,19 @@ class MessageSaver {
 	private static Logger logger = LoggerFactory.getLogger(MessageSaver.class);
 
 	static void append(File inlFile, Message message, String textMessage) {
-		write(inlFile, textMessage, CREATE, APPEND);
+		write(inlFile, textMessage);
 		saveAttachments(inlFile.getParentFile(), message);
 	}
 
 	static void save(File inlFile, List<Message> messages) {
 		StringBuilder builder = new StringBuilder();
 		for (Message m : messages) builder.append(m.toString()).append("\n\n");
-		write(inlFile, builder.toString(), CREATE);
+		write(inlFile, builder.toString());
 	}
 
-	static synchronized void write(File inlFile, String textMessage, StandardOpenOption... option) {
+	private static synchronized void write(File inlFile, String textMessage) {
 		try {
-			Files.write(inlFile.toPath(), (textMessage + "\n\n").getBytes(), option);
+			Files.write(inlFile.toPath(), (textMessage + "\n\n").getBytes(), CREATE, APPEND);
 		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		}
