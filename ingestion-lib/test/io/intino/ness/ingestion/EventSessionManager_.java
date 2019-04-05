cba178208e8e3dfcce1c6ea08eff6f7e7c1978f3
Jose Évora <jose.evora@siani.es>
2019-04-05 11:00:59 +0100

    test after correct generation

--- ingestion-lib/test/io/intino/ness/ingestion/EventSessionManager_.java
+++ ingestion-lib/test/io/intino/ness/ingestion/EventSessionManager_.java
@@ -3,6 +3,7 @@ package io.intino.ness.ingestion;
 import io.intino.alexandria.Scale;
 import io.intino.alexandria.Timetag;
 import io.intino.alexandria.inl.Message;
+import io.intino.alexandria.zim.ZimReader;
 import io.intino.ness.datalake.FileDatalake;
 import org.junit.After;
 import org.junit.Test;
@@ -11,6 +12,10 @@ import java.io.File;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class EventSessionManager_ {
 
@@ -24,14 +29,23 @@ public class EventSessionManager_ {
 	public void should_create_an_event_session() {
 		SessionHandler handler = new SessionHandler();
 		EventSession session = handler.createEventSession();
-		for (int i = 0; i < 100; i++) {
-			LocalDateTime now = LocalDateTime.now();
-			session.put("tank1", new Timetag(now, Scale.Day), message(now.toInstant(ZoneOffset.UTC), i));
+		List<Message> messageList = new ArrayList<>();
+		for (int i = 0; i < 30; i++) {
+			LocalDateTime now = LocalDateTime.of(2019, 02, 28, 16, 15 + i);
+			Message message = message(now.toInstant(ZoneOffset.UTC), i);
+			messageList.add(message);
+			session.put("tank1", new Timetag(now, Scale.Hour), message);
 		}
 		session.close();
 		Digester digester = new Digester(new FileDatalake(new File("temp/datalake")), new File("temp/session"));
 		digester.push(handler.sessions());
 		digester.seal();
+		ZimReader reader = new ZimReader(new File("temp/datalake/events/tank1/2019022816.zim"));
+		for (int i = 0; i < 30; i++) {
+			Message next = reader.next();
+			assertEquals(next.get("ts"), messageList.get(i).get("ts"));
+			assertEquals(next.get("entries"), messageList.get(i).get("entries"));
+		}
 	}
 
 	private Message message(Instant instant, int index) {
