fafb145147317f7cedb2ca22a8041942d5dcaff2
Octavio Roncal <octavioroncal@monentia.es>
2019-08-08 11:06:53 +0100

    updated test

--- data-hub-test/src/org/example/datahubtest/box/DataHubTestBox.java
+++ data-hub-test/src/org/example/datahubtest/box/DataHubTestBox.java
@@ -1,6 +1,14 @@
 package org.example.datahubtest.box;
 
+import io.intino.datahub.DataHub;
+import io.intino.datahub.graph.NessGraph;
+import io.intino.tara.magritte.Graph;
+
+import java.io.File;
+
 public class DataHubTestBox extends AbstractBox {
+
+	private DataHub dataHub;
 
 	public DataHubTestBox(String[] args) {
 		super(args);
@@ -13,11 +21,19 @@ public class DataHubTestBox extends AbstractBox {
 	@Override
 	public io.intino.alexandria.core.Box put(Object o) {
 		super.put(o);
+		if (o instanceof Graph)
+			dataHub = new DataHub(((Graph) o).as(NessGraph.class), new File(configuration.stagePath()));
 		return this;
 	}
 
 	public io.intino.alexandria.core.Box open() {
-		return super.open();
+		super.open();
+		dataHub.start();
+		return this;
+	}
+
+	public DataHub dataHub() {
+		return dataHub;
 	}
 
 	public void close() {
--- data-hub-test/src/org/example/datahubtest/box/Main.java
+++ data-hub-test/src/org/example/datahubtest/box/Main.java
@@ -4,10 +4,7 @@ import io.intino.alexandria.core.Box;
 
 public class Main {
 	public static void main(String[] args) {
-		Box box = new DataHubTestBox(args);
-		io.intino.tara.magritte.Graph graph = new io.intino.tara.magritte.Graph().loadStashes("DataHubTest");
-		box.put(graph);
-		box.open();
+		Box box = new DataHubTestBox(args).put(new io.intino.tara.magritte.Graph().loadStashes("solution")).open();
 		Runtime.getRuntime().addShutdownHook(new Thread(box::close));
 	}
 }
