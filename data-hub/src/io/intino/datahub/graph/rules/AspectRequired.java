fe20dc20c966ec2882ad28b6596c9941455bb5fe
Octavio Roncal <octavioroncal@monentia.es>
2019-11-06 13:54:05 +0000

    updated to new tara.

--- data-hub/src/io/intino/datahub/graph/rules/AspectRequired.java
+++ data-hub/src/io/intino/datahub/graph/rules/AspectRequired.java
@@ -3,11 +3,10 @@ package io.intino.datahub.graph.rules;
 import io.intino.tara.lang.model.Node;
 import io.intino.tara.lang.model.rules.NodeRule;
 
-public class FacetRequired implements NodeRule {
+public class AspectRequired implements NodeRule {
 	public boolean accept(Node node) {
-		return !node.facets().isEmpty();
+		return !node.appliedAspects().isEmpty();
 	}
-
 
 	@Override
 	public String errorMessage() {
