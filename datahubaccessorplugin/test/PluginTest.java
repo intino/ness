5dbb0a672a705496ad32dd051cd366d3f8405795
Octavio Roncal <octavioroncal@monentia.es>
2019-11-18 08:31:43 +0000

    test

--- datahubaccessorplugin/test/PluginTest.java
+++ datahubaccessorplugin/test/PluginTest.java
@@ -12,8 +12,8 @@ public class PluginTest {
 	public void should_build_accessors() {
 		DataHubAccessorsPluginLauncher launcher = new DataHubAccessorsPluginLauncher();
 		launcher.
-				moduleStructure(new PluginLauncher.ModuleStructure(Collections.singletonList(new File("/Users/oroncal/workspace/ness/data-hub-test/src")), Collections.singletonList(new File("/Users/oroncal/workspace/ness/data-hub-test/res")), new File("/Users/oroncal/workspace/ness/out/data-hub-test/")))
-				.systemProperties(new PluginLauncher.SystemProperties(new File("/Applications/IntelliJ IDEA.app/Contents/plugins/maven/lib/maven3"), new File("/Library/Java/JavaVirtualMachines/jdk-11.0.4.jdk/Contents/Home")))
+				moduleStructure(new PluginLauncher.ModuleStructure(Collections.singletonList(new File(System.getProperty("user.home") + "/workspace/ness/data-hub-test/src")), Collections.singletonList(new File(System.getProperty("user.home") + "/workspace/ness/data-hub-test/res")), new File("/Users/oroncal/workspace/ness/out/data-hub-test/")))
+				.systemProperties(new PluginLauncher.SystemProperties(new File("/Applications/IntelliJ IDEA - 2019.2.app/Contents/plugins/maven/lib/maven3/"), new File("/Library/Java/JavaVirtualMachines/jdk-11.0.4.jdk/Contents/Home")))
 				.logger(System.out)
 				.moduleConfiguration(new Graph().loadStashes("data-hub-test").as(LegioGraph.class));
 		launcher.run();
