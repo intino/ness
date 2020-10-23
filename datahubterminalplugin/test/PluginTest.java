20c26c66fa720f8d0f32ba383839c5f342332776
Octavio Roncal <octavioroncal@monentia.es>
2020-10-23 10:10:41 +0100

    ignore test

--- datahubterminalplugin/test/PluginTest.java
+++ datahubterminalplugin/test/PluginTest.java
@@ -2,6 +2,7 @@ import io.intino.Configuration;
 import io.intino.ness.datahubterminalplugin.DataHubTerminalsPluginLauncher;
 import io.intino.plugin.PluginLauncher;
 import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -10,6 +11,8 @@ import java.time.Instant;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
+
+@Ignore
 public class PluginTest {
 	@Test
 	public void should_build_test_terminals() throws IOException {
