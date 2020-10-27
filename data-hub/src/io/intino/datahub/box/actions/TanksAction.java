e097896779cfbcc6d71680fbf86f0d399136231e
Octavio Roncal <octavioroncal@monentia.es>
2020-10-27 16:03:06 +0000

    updated dependencies

--- data-hub/src/io/intino/datahub/box/actions/TanksAction.java
+++ data-hub/src/io/intino/datahub/box/actions/TanksAction.java
@@ -10,7 +10,7 @@ public class TanksAction {
 	public DataHubBox box;
 	public io.intino.alexandria.Context context = new io.intino.alexandria.Context();
 
-	public String execute() {
+	public java.lang.String execute() {
 		String collect = box.datalake().eventStore().tanks().map(Datalake.EventStore.Tank::name).collect(joining("\n"));
 		return collect.isEmpty() ? "No tanks yet" : collect;
 	}
