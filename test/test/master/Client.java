93c51408e1266a491b59f49215b658f1687324b7
cherrera <naitsirc98@gmail.com>
2022-11-14 14:43:34 +0000

    master: add enabled property to Entity objects. datahubterminalplugin: refactor publishing and add EntityListeners

--- test/test/master/Client.java
+++ test/test/master/Client.java
@@ -1,19 +1,14 @@
 package master;
 
-import com.hazelcast.core.EntryAdapter;
-import com.hazelcast.core.EntryEvent;
 //import org.example.test.model.FullLoadMasterTerminal;
+
+import org.example.test.model.MasterTerminal;
 
 public class Client {
 
 	public static void main(String[] args) {
 
-//		t.addEntryListener(new EntryAdapter<>() {
-//			@Override
-//			public void entryAdded(EntryEvent<String, String> event) {
-//				super.entryAdded(event);
-//			}
-//		});
+		MasterTerminal terminal = MasterTerminal.create();
 
 	}
 }
