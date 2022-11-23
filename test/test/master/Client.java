b9377a57308e292297ce790e048d627fe87db2d8
cherrera <naitsirc98@gmail.com>
2022-11-23 12:32:53 +0000

    datahubterminalplugin: add listeners to the MastertTerminal lifecycle.

--- test/test/master/Client.java
+++ test/test/master/Client.java
@@ -1,43 +1,19 @@
 package master;
 
-import io.intino.ness.master.messages.MasterMessageException;
-import io.intino.ness.master.messages.Response;
-import io.intino.ness.master.messages.listeners.EntityListener;
-import io.intino.ness.master.messages.listeners.ErrorListener;
 import org.example.test.model.MasterTerminal;
-import org.example.test.model.entities.Employee;
-import org.example.test.model.entities.Theater;
 
-import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.stream.Stream;
 
 public class Client {
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-		MasterTerminal terminal = MasterTerminal.create();
+		MasterTerminal.Config config = new MasterTerminal.Config();
+		config.putProperty("hazelcast.socket.connect.timeout.seconds", "1");
+
+		MasterTerminal terminal = MasterTerminal.create(config);
 		terminal.start();
 
-		Employee e = new Employee("", terminal);
 
-		Theater theater = terminal.disabled().theater("123:theater");
-		Stream<Theater> theaters = terminal.disabled().theaters();
-		List<Theater> theaterList = terminal.disabled().theaterList();
-
-		Future<Response<Employee>> future = terminal.publish(new Employee("123:employee", terminal));
-
-		Response<Employee> response = future.get();
-
-		if(response.success()) {
-			EntityListener.Event<Employee> event = response.event();
-			Employee entity = event.entity();
-			// ...
-		} else {
-			ErrorListener.Error error = response.error();
-			MasterMessageException cause = error.cause();
-			// ...
-		}
 	}
 }
--- test/test/master/general/MasterClient_.java
+++ test/test/master/general/MasterClient_.java
@@ -1,5 +1,10 @@
 package master.general;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.LifecycleEvent;
+import com.hazelcast.core.LifecycleListener;
 import io.intino.ness.master.messages.Response;
 import org.example.test.model.MasterTerminal;
 import org.example.test.model.entities.Employee;
@@ -11,34 +16,106 @@ import java.util.concurrent.Future;
 public class MasterClient_ {
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
-		MasterTerminal.Config config = new MasterTerminal.Config()
-				.clientName("the client")
-				.allowWriting(true)
-				.putProperty("hazelcast.logging.type", "none")
-				.addresses(List.of("localhost:62555"));
 
-		MasterTerminal terminal = MasterTerminal.create(config);
-		terminal.start();
+		ClientConfig config = new ClientConfig();
+		config.getNetworkConfig().addAddress("localhost:62555");
+		config.getConnectionStrategyConfig().getConnectionRetryConfig()
+				.setClusterConnectTimeoutMillis(1000);
 
-		Runtime.getRuntime().addShutdownHook(new Thread(terminal::stop));
+		HazelcastInstance hz = HazelcastClient.newHazelcastClient(config);
 
-		terminal.publish(new Employee("1:employee", terminal).name("Pedri"));
-		terminal.publish(new Employee("2:employee", terminal).name("Gavi"));
-		terminal.publish(new Employee("3:employee", terminal).name("Asensio"));
-		terminal.publish(new Employee("4:employee", terminal).name("Ansu Fati"));
-		terminal.publish(new Employee("5:employee", terminal).name("Nico Williams"));
+		hz.getLifecycleService().addLifecycleListener(new LifecycleListener() {
+			@Override
+			public void stateChanged(LifecycleEvent event) {
+				System.out.println(event);
+			}
+		});
 
-		Future<Response<Employee>> future = terminal.disable("1:employee");
+		Runtime.getRuntime().addShutdownHook(new Thread(hz::shutdown));
 
-		future.get();
+//		MasterTerminal.Config config = new MasterTerminal.Config()
+//				.clientName("the client")
+//				.allowWriting(true)
+//				.addresses(List.of("localhost:62555"))
+//				.putProperty("hazelcast.logging.type", "none")
+//				.putProperty("hazelcast.socket.connect.timeout.seconds", "1");
+//
+//		MasterTerminal terminal = MasterTerminal.create(config);
+//		terminal.start();
+//
+//		Runtime.getRuntime().addShutdownHook(new Thread(terminal::stop));
 
-		System.out.println("1 => " + terminal.employee("1:employee"));
+//		terminal.publish(new Employee("1:employee", terminal).name("Pedri"));
+//		terminal.publish(new Employee("2:employee", terminal).name("Gavi"));
+//		terminal.publish(new Employee("3:employee", terminal).name("Asensio"));
+//		terminal.publish(new Employee("4:employee", terminal).name("Ansu Fati"));
+//		terminal.publish(new Employee("5:employee", terminal).name("Nico Williams"));
+//
+//		Future<Response<Employee>> future = terminal.disable("1:employee");
+//
+//		future.get();
+//
+//		System.out.println("1 => " + terminal.employee("1:employee"));
+//
+//		System.out.println("2 => " + terminal.disabled().employee("1:employee"));
+//
+//		terminal.publish(new Employee("6:employee", terminal).name("Unai Simon"));
+//		terminal.publish(new Employee("7:employee", terminal).name("Morata"));
+//
+//		System.out.println("done");
+	}
 
-		System.out.println("2 => " + terminal.disabled().employee("1:employee"));
+	public static class ConnectionConfig {
 
-		terminal.publish(new Employee("6:employee", terminal).name("Unai Simon"));
-		terminal.publish(new Employee("7:employee", terminal).name("Morata"));
+		private int initialBackoffMillis = 1000;
+		private int maxBackoffMillis = 30000;
+		private float multiplier = 1.05f;
+		private int clusterConnectTimeoutMillis = - 1;
+		private float jitter = 0;
 
-		System.out.println("done");
+		public int initialBackoffMillis() {
+			return initialBackoffMillis;
+		}
+
+		public ConnectionConfig initialBackoffMillis(int initialBackoffMillis) {
+			this.initialBackoffMillis = initialBackoffMillis;
+			return this;
+		}
+
+		public int maxBackoffMillis() {
+			return maxBackoffMillis;
+		}
+
+		public ConnectionConfig maxBackoffMillis(int maxBackoffMillis) {
+			this.maxBackoffMillis = maxBackoffMillis;
+			return this;
+		}
+
+		public float multiplier() {
+			return multiplier;
+		}
+
+		public ConnectionConfig multiplier(float multiplier) {
+			this.multiplier = multiplier;
+			return this;
+		}
+
+		public int clusterConnectTimeoutMillis() {
+			return clusterConnectTimeoutMillis;
+		}
+
+		public ConnectionConfig clusterConnectTimeoutMillis(int clusterConnectTimeoutMillis) {
+			this.clusterConnectTimeoutMillis = clusterConnectTimeoutMillis;
+			return this;
+		}
+
+		public float jitter() {
+			return jitter;
+		}
+
+		public ConnectionConfig jitter(float jitter) {
+			this.jitter = jitter;
+			return this;
+		}
 	}
 }
