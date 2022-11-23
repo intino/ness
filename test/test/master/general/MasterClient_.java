b9e66454a19ec81a25b8328a2347edfad3a8ec6a
cherrera <naitsirc98@gmail.com>
2022-11-23 14:30:57 +0000

    datahubterminalplugin: add listeners to the MasterTerminal lifecycle.

--- test/test/master/general/MasterClient_.java
+++ test/test/master/general/MasterClient_.java
@@ -5,6 +5,7 @@ import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.LifecycleEvent;
 import com.hazelcast.core.LifecycleListener;
+import io.intino.ness.master.core.MasterLifecycleEvent;
 import io.intino.ness.master.messages.Response;
 import org.example.test.model.MasterTerminal;
 import org.example.test.model.entities.Employee;
@@ -13,56 +14,64 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import static io.intino.ness.master.core.MasterLifecycleEvent.State.CLIENT_DISCONNECTED;
+
 public class MasterClient_ {
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-		ClientConfig config = new ClientConfig();
-		config.getNetworkConfig().addAddress("localhost:62555");
-		config.getConnectionStrategyConfig().getConnectionRetryConfig()
-				.setClusterConnectTimeoutMillis(1000);
+//		ClientConfig config = new ClientConfig();
+//		config.getNetworkConfig().addAddress("localhost:62555");
+//		config.getConnectionStrategyConfig().getConnectionRetryConfig()
+//				.setClusterConnectTimeoutMillis(1000);
+//
+//		HazelcastInstance hz = HazelcastClient.newHazelcastClient(config);
+//
+//		hz.getLifecycleService().addLifecycleListener(new LifecycleListener() {
+//			@Override
+//			public void stateChanged(LifecycleEvent event) {
+//				System.out.println(event);
+//			}
+//		});
+//
+//		Runtime.getRuntime().addShutdownHook(new Thread(hz::shutdown));
 
-		HazelcastInstance hz = HazelcastClient.newHazelcastClient(config);
+		MasterTerminal.Config config = new MasterTerminal.Config()
+				.clientName("the client")
+				.allowWriting(true)
+				.addresses(List.of("localhost:62555"))
+				.putProperty("hazelcast.logging.type", "none")
+				.putProperty("hazelcast.socket.connect.timeout.seconds", "1");
 
-		hz.getLifecycleService().addLifecycleListener(new LifecycleListener() {
-			@Override
-			public void stateChanged(LifecycleEvent event) {
-				System.out.println(event);
+		MasterTerminal terminal = MasterTerminal.create(config);
+		terminal.addLifecycleListener(event -> {
+			if(event.state() == CLIENT_DISCONNECTED) {
+				// ...
 			}
 		});
 
-		Runtime.getRuntime().addShutdownHook(new Thread(hz::shutdown));
+		terminal.start();
 
-//		MasterTerminal.Config config = new MasterTerminal.Config()
-//				.clientName("the client")
-//				.allowWriting(true)
-//				.addresses(List.of("localhost:62555"))
-//				.putProperty("hazelcast.logging.type", "none")
-//				.putProperty("hazelcast.socket.connect.timeout.seconds", "1");
-//
-//		MasterTerminal terminal = MasterTerminal.create(config);
-//		terminal.start();
-//
-//		Runtime.getRuntime().addShutdownHook(new Thread(terminal::stop));
+		Runtime.getRuntime().addShutdownHook(new Thread(terminal::stop));
 
-//		terminal.publish(new Employee("1:employee", terminal).name("Pedri"));
-//		terminal.publish(new Employee("2:employee", terminal).name("Gavi"));
-//		terminal.publish(new Employee("3:employee", terminal).name("Asensio"));
-//		terminal.publish(new Employee("4:employee", terminal).name("Ansu Fati"));
-//		terminal.publish(new Employee("5:employee", terminal).name("Nico Williams"));
-//
-//		Future<Response<Employee>> future = terminal.disable("1:employee");
-//
-//		future.get();
-//
-//		System.out.println("1 => " + terminal.employee("1:employee"));
-//
-//		System.out.println("2 => " + terminal.disabled().employee("1:employee"));
-//
-//		terminal.publish(new Employee("6:employee", terminal).name("Unai Simon"));
-//		terminal.publish(new Employee("7:employee", terminal).name("Morata"));
-//
-//		System.out.println("done");
+		terminal.publish(new Employee("1:employee", terminal).name("Pedri"));
+		terminal.publish(new Employee("2:employee", terminal).name("Gavi"));
+		terminal.publish(new Employee("3:employee", terminal).name("Asensio"));
+		terminal.publish(new Employee("4:employee", terminal).name("Ansu Fati"));
+		terminal.publish(new Employee("5:employee", terminal).name("Nico Williams"));
+
+		Future<Response<Employee>> future = terminal.disable("1:employee");
+
+		future.get();
+
+		System.out.println("1 => " + terminal.employee("1:employee"));
+
+		System.out.println("2 => " + terminal.disabled().employee("1:employee"));
+
+		terminal.publish(new Employee("6:employee", terminal).name("Unai Simon"));
+		terminal.publish(new Employee("7:employee", terminal).name("Morata"));
+
+		System.out.println("done");
 	}
 
 	public static class ConnectionConfig {
