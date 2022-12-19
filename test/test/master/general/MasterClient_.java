9e1b98355cb503cb14caf1774b09517822e518a8
cherrera <naitsirc98@gmail.com>
2022-12-19 08:36:46 +0000

    datahubterminalplugin: implicitly include child entity classes when specifying parent entities in publish/subscribe during terminal definition.

--- test/test/master/general/MasterClient_.java
+++ test/test/master/general/MasterClient_.java
@@ -3,23 +3,22 @@ package master.general;
 import io.intino.alexandria.terminal.JmsConnector;
 import io.intino.test.datahubtest.TestTerminal;
 import io.intino.test.datahubtest.master.Entities;
-import io.intino.test.datahubtest.master.entities.Zone;
+import io.intino.test.datahubtest.master.entities.LogModule;
 
 import java.io.File;
-import java.util.List;
 
 public class MasterClient_ {
 
 	public static void main(String[] args) {
 		TestTerminal terminal = createTerminal();
 		Entities entities = terminal.entities();
-		List<Zone> zones = entities.zoneList();
-		System.out.println(zones);
+		LogModule logModule = entities.logModule("123");
+		entities.publish(new LogModule("123").name("hola"));
 	}
 
 	private static TestTerminal createTerminal() {
 		JmsConnector connector = new JmsConnector(
-				"localhost:62123",
+				"failover:(tcp://localhost:62123)",
 				"test",
 				"test",
 				"test",
