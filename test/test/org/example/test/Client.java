b57ca0353db6a574162d1cbb77babb94218dd5aa
Octavio Roncal <octavioroncal11@gmail.com>
2023-03-17 08:28:08 +0000

    added ssl security

--- test/test/org/example/test/Client.java
+++ test/test/org/example/test/Client.java
@@ -8,12 +8,34 @@ import io.intino.test.datahubtest.TestTerminal;
 import io.intino.test.datahubtest.datamarts.master.MasterDatamart;
 import io.intino.test.datahubtest.datamarts.master.entities.JavaApplication;
 import io.intino.test.datahubtest.datamarts.master.entities.User;
+import org.apache.activemq.ActiveMQSslConnectionFactory;
+import org.junit.Ignore;
+import org.junit.Test;
 
+import javax.jms.Connection;
+import javax.jms.Session;
 import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
 
 public class Client {
+
+	@Test
+	@Ignore
+	public void sslTest() throws Exception {
+		ActiveMQSslConnectionFactory connectionFactory = new ActiveMQSslConnectionFactory("ssl://localhost:63000");
+		connectionFactory.setKeyStore(new File("../temp/datahub/client.jks").getAbsolutePath());
+		connectionFactory.setTrustStore(new File("../temp/datahub/client.jts").getAbsolutePath());
+		connectionFactory.setKeyStorePassword("vaquep0");
+		connectionFactory.setTrustStorePassword("vaquep0");
+		connectionFactory.setUserName("consul");
+		connectionFactory.setPassword("consul");
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		session.close();
+
+	}
 
 	static MessageEvent event = new MessageEvent(new Message("UserAssertion")
 			.set("ss", "test")
