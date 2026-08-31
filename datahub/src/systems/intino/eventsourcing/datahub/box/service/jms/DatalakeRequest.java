d7612c6b4d0e3357f1a6e6632905da693668326f
Octavio Roncal <octavio.roncal@ulpgc.es>
2026-08-31 10:11:07 +0100

    fixed datahub

--- datahub/src/systems/intino/eventsourcing/datahub/box/service/jms/DatalakeRequest.java
+++ datahub/src/systems/intino/eventsourcing/datahub/box/service/jms/DatalakeRequest.java
@@ -9,7 +9,8 @@ import org.apache.activemq.command.ActiveMQBytesMessage;
 import systems.intino.eventsourcing.datahub.box.DatahubBox;
 import systems.intino.eventsourcing.datalake.Datalake;
 import systems.intino.eventsourcing.datalake.file.message.MessageEventTub;
-import systems.intino.eventsourcing.event.message.MessageEvent;
+import systems.intino.eventsourcing.datalake.file.resource.ResourceEventTub;
+import systems.intino.eventsourcing.event.Event;
 import systems.intino.eventsourcing.jms.MessageReader;
 
 import java.io.File;
@@ -43,9 +44,8 @@ public class DatalakeRequest {
 
 	private Stream<Message> handleDatalakeDownload(String request) {
 		if (request.equals("[Datalake]")) return Stream.of(toJmsMessage((box.datalake()).root().getAbsolutePath()));
-		if (request.equals("messageStore/tanks"))
-			return Stream.of(toJmsMessage(Json.toString(box.datalake().messageStore().tanks()
-					.map(DatalakeRequest::tankOf).collect(toList()))));
+		if (request.equals("messageStore/tanks")) return tankResponse(box.datalake().messageStore());
+		if (request.equals("resourceStore/tanks")) return tankResponse(box.datalake().resourceStore());
 		if (request.startsWith("{")) {
 			JsonObject jsonObject = Json.fromString(request, JsonObject.class);
 			if ("reflow".equals(jsonObject.get("operation").getAsString())) return reflow(jsonObject);
@@ -53,11 +53,16 @@ public class DatalakeRequest {
 		return Stream.empty();
 	}
 
+	private static <T extends Event> Stream<Message> tankResponse(Datalake.Store<T> store) {
+		return Stream.of(toJmsMessage(Json.toString(store.tanks().map(DatalakeRequest::tankOf).collect(toList()))));
+	}
+
 	private Stream<Message> reflow(JsonObject request) {
 		String tank = request.get("tank").getAsString();
+		String source = request.has("source") ? request.get("source").getAsString() : null;
 		List<String> tubs = new ArrayList<>();
 		request.get("tubs").getAsJsonArray().forEach(v -> tubs.add(v.getAsString()));
-		List<File> files = filesOf(tank, tubs).toList();
+		List<File> files = filesOf(tank, source, tubs).toList();
 		return IntStream.range(0, files.size())
 				.mapToObj(i -> toMessage(read(files.get(i)), i < files.size() - 1))
 				.filter(Objects::nonNull);
@@ -84,23 +89,59 @@ public class DatalakeRequest {
 		}
 	}
 
-	private Stream<File> filesOf(String tank, List<String> tubs) {
-		return box.datalake().messageStore().tank(tank).sources().flatMap(Datalake.Store.Source::tubs).filter(t -> tubs.contains(t.timetag().value())).map(t -> ((MessageEventTub) t).file());
+	private Stream<File> filesOf(String tank, String source, List<String> tubs) {
+		if (box.datalake().messageStore().containsTank(tank)) return messageFilesOf(tank, source, tubs);
+		if (box.datalake().resourceStore().containsTank(tank)) return resourceFilesOf(tank, source, tubs);
+		return Stream.empty();
 	}
 
-	private static Tank tankOf(Datalake.Store.Tank<MessageEvent> t) {
-		return new Tank(t.name(), t.scale().name(), t.sources().map(Datalake.Store.Source::name).collect(toList()));
+	private Stream<File> messageFilesOf(String tank, String source, List<String> tubs) {
+		return sourcesOf(box.datalake().messageStore().tank(tank), source)
+				.flatMap(Datalake.Store.Source::tubs)
+				.filter(t -> tubs.contains(t.timetag().value()))
+				.map(t -> ((MessageEventTub) t).file());
+	}
+
+	private Stream<File> resourceFilesOf(String tank, String source, List<String> tubs) {
+		return sourcesOf(box.datalake().resourceStore().tank(tank), source)
+				.flatMap(Datalake.Store.Source::tubs)
+				.filter(t -> tubs.contains(t.timetag().value()))
+				.map(t -> ((ResourceEventTub) t).file());
+	}
+
+	private static <T extends Event> Stream<Datalake.Store.Source<T>> sourcesOf(Datalake.Store.Tank<T> tank, String source) {
+		Stream<Datalake.Store.Source<T>> sources = tank.sources();
+		return source == null || source.isEmpty() ? sources : sources.filter(s -> source.equals(s.name()));
+	}
+
+	private static <T extends Event> Tank tankOf(Datalake.Store.Tank<T> tank) {
+		String scale = tank.scale() != null ? tank.scale().name() : null;
+		return new Tank(tank.name(), scale, tank.sources().map(DatalakeRequest::sourceOf).collect(toList()));
+	}
+
+	private static <T extends Event> Source sourceOf(Datalake.Store.Source<T> source) {
+		return new Source(source.name(), source.tubs().map(t -> t.timetag().value()).collect(toList()));
 	}
 
 	private static class Tank {
 		String name;
 		String scale;
-		List<String> sources;
+		List<Source> sources;
 
-		public Tank(String name, String scale, List<String> sources) {
+		public Tank(String name, String scale, List<Source> sources) {
 			this.name = name;
 			this.scale = scale;
 			this.sources = sources;
+		}
+	}
+
+	private static class Source {
+		String name;
+		List<String> tubs;
+
+		public Source(String name, List<String> tubs) {
+			this.name = name;
+			this.tubs = tubs;
 		}
 	}
 }
